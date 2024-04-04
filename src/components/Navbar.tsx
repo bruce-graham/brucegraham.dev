991d67d41ae01e25a5c67b831b53486200a4a844
bruce-graham <brucegraham@gmx.com>
2024-04-03 20:15:03 -0700

    Add src/components/ which initially contails footer and navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+
+
+  return (
+    <nav className="navbar" role="navigation">
+      <div>Bruce Graham</div>
+      <div className="nav-link">
+        <Link to="/">Home</Link>
+      </div>
+      <div className="nav-link">
+        <Link to="/About">About</Link>
+      </div>
+      <div className="nav-link">
+        <Link to="/Resume">Resume</Link>
+      </div>
+      <div className="nav-link">
+        <Link to="/Contact">Contact</Link>
+      </div>
+      <hr></hr>
+    </nav>
+  )
+}
+
+export default Navbar
