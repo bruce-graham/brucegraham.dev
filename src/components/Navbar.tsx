da366a086d807a9ab05867c432c0213e164f2205
bruce-graham <brucegraham@gmx.com>
2024-04-03 20:24:39 -0700

    add intial navbar content

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,6 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 function Navbar() {
-
-
   return (
     <nav className="navbar" role="navigation">
       <div>Bruce Graham</div>
