ceca95b4dc91c5ccfe7a8e426f923730c988c5ff
bruce-graham <brucegraham@gmx.com>
2024-04-03 20:17:24 -0700

    Add initial server file

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-const app = express();
-const port = 8888;
+import express from 'express'
+const app = express()
+const port = 8888
 
 app.get('/', (req, res) => {
-	res.send('hello world');
-});
+  res.send('hello world')
+})
 
 app.listen(port, () => {
-	console.log(`example app listenting on ${port}`)
-});
+  console.log(`example app listenting on ${port}`)
+})
